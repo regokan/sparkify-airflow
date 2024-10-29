6d7bf0f01b1633e2feec74235dccd3288517f600
regokan <regokan@gmail.com>
2024-10-29 08:21:27 +0530

    fix: update sql queries in fact table to properly insert data into redshift

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -8,12 +9,20 @@ from helpers.sql_queries import SqlQueries
 class LoadFactOperator(BaseOperator):
 
     ui_color = "#F98866"
+    template_fields: Sequence[str] = (
+        "redshift_conn_id",
+        "aws_credentials_id",
+        "table_name",
+        "columns",
+    )
 
     @apply_defaults
     def __init__(
         self,
-        redshift_conn_id="redshift",
-        aws_credentials_id="aws_credentials",
+        redshift_conn_id: str,
+        aws_credentials_id: str,
+        table_name: str,
+        columns: list,
         *args,
         **kwargs,
     ):
@@ -22,10 +31,14 @@ class LoadFactOperator(BaseOperator):
         Args:
             - redshift_conn_id: The connection ID for the Redshift cluster
             - aws_credentials_id: The connection ID for the AWS credentials
+            - table_name: The target table to insert data into
+            - columns: A list of columns to match the table schema
         """
         super().__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.aws_credentials_id = aws_credentials_id
+        self.table_name = table_name
+        self.columns = columns
 
     def execute(self, context):
         self.log.info("Starting to load data into the songplays fact table")
@@ -43,10 +56,15 @@ class LoadFactOperator(BaseOperator):
             postgres_conn_id=self.redshift_conn_id, extra_dejson=extra_dejson
         )
 
+        insert_query = f"""
+            INSERT INTO {self.table_name} ({",".join(self.columns)})
+            {SqlQueries.songplay_table_insert}
+        """
+
         # Execute the SQL query to load data into the songplays fact table
         try:
             self.log.info("Executing fact table load SQL")
-            redshift.run(SqlQueries.songplay_table_insert)
+            redshift.run(insert_query)
 
             self.log.info("Successfully loaded data into the songplays fact table")
         except Exception as e:
